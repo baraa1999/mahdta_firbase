56a9a860b124794d0325070eaec7d9761d5b235f
baraa1999 <baraaalian7@gmail.com>
2023-07-26 19:56:30 +0300

    add firebase to all platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mahadta_firbase/pages/logain_page.dart';
 import 'package:mahadta_firbase/pages/register_page.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ScollerCaht());
 }
 
