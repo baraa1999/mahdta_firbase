7a3ceac2ee4abfd46cb40d02d26b796315296126
baraa1999 <baraaalian7@gmail.com>
2023-07-27 14:15:15 +0300

    form validation

--- lib/widget/custom_button.dart
+++ lib/widget/custom_button.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget {
-  CustomButton({this.onTap, required this.text});
-  String text;
+class CustomButon extends StatelessWidget {
+  CustomButon({this.onTap, required this.text});
   VoidCallback? onTap;
-
+  String text;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -15,8 +14,10 @@ class CustomButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(8),
         ),
         width: double.infinity,
-        height: 65,
-        child: Center(child: Text(text)),
+        height: 60,
+        child: Center(
+          child: Text(text),
+        ),
       ),
     );
   }
