43a684d41a41b61c5e61f00e2c0e291b3b92de6c
baraa1999 <baraaalian7@gmail.com>
2023-08-02 14:39:02 +0300

    expanded widget in chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -25,10 +25,33 @@ class ChatPage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return const ChatBubule();
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return const ChatBubule();
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: ('Send Message'),
+                  prefix: const Icon(
+                    Icons.send,
+                    color: kPrimaryColor,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: const BorderSide(color: kPrimaryColor))),
+            ),
+          ),
+        ],
       ),
     );
   }
